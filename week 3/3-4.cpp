295465aba13e9c7b956e2c177506eca4f9490637
moonsub <131600659+moonsub0424@users.noreply.github.com>
2026-03-21 18:30:53 +0900

    week3 HW

---
+++ week 3/3-4.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <ios>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	vector<string> strs;
+	string str;
+
+	while (getline(cin, str)) {
+		strs.push_back(str);
+	}
+
+	if (strs.size() == 0) {
+		cout << "입력된 문장이 없습니다." << endl;
+		
+		return 0;
+	}
+
+	int max = strs[0].length();
+	int min = strs[0].length();
+
+	for (int i = 1; i < strs.size(); i++) {
+		int current = strs[i].length();
+
+		if (current > max) {
+			max = current;
+		}
+		if (current < min) {
+			min = current;
+		}
+
+	}
+
+	cout << "가장 길이가 긴 문자열의 길이: " << max << "입니다." << endl;
+	cout << "가장 길이가 짧은 문자열의 길이: " << min << "입니다." << endl;
+
+
+	return 0;
+}
