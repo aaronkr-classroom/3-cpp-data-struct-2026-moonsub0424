55d3b83b7dd73251e7f86c95e8b9ebe07f7dd085
ththfql <131600659+moonsub0424@users.noreply.github.com>
2026-03-25 15:24:56 +0900

    example

---
+++ week 3/str_size.cpp
@@ -0,0 +1,31 @@
+﻿// str_size.cpp
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	// 프로그램 설명 하기
+	cout << "Enter all words, then EOF: ";
+
+	// 입력 받기
+	string word;
+	string max = "";
+	string min = "";
+
+	while (cin >> word) {
+		if (min.size() == 0) min = word;
+		if (max.size() == 0) max = word;
+
+		//크기 확인
+		if (word.size() < min.size()) min = word;
+		else if (word.size() > max.size()) max = word;
+	}
+
+	cout <<
+		"Min word: " << min << "size: " << min.size() << endl <<
+		"Max word: " << max << "size: " << max.size() << endl;
+
+	return 0;
+
+}
---
+++ week 3/word_count.cpp
@@ -0,0 +1,31 @@
+﻿// word_count.cpp
+#include <iostream>
+#include <string>
+#include <ios>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	//프로그램 설명 출력
+	cout << "Enter all words, then E0F: "; //End of File
+	vector<string> words;
+
+	// 입력 받기
+	string word;
+	while (cin >> word) {
+		words.push_back(word);
+	}
+
+	// { word: count } 출력
+	for (int i = 0; i < words.size(); i++) {
+		cout << words[i] << ": " <<
+			count(words.begin(), words.end(), words[i]) << endl;
+	}
+
+	// 마지막 결과
+	cout << "Words: " << words.size();
+
+	return 0;
+}
